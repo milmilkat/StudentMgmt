e984c5d93a95080fe4d32a06769261a592871795
katebi.milad <katebi.milad@yahoo.com>
2020-11-23 10:02:21 +1300

    Add project files.

---
+++ StudentMgmt/Migrations/20201122205314_SMC.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentMgmt.Migrations
+{
+    public partial class SMC : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
---
+++ StudentMgmt/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentMgmt.Models
+{
+    public class Student
+    {
+        public int Id { get; set; } // this is the primary key
+        public string FirstName { get; set; } // the first name of the student
+        public string LastName { get; set; } // the last name of the student
+    }
+}
